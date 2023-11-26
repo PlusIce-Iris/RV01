72f90836e1c95b8758c428045a397cf641c0d412
PlusIce-Iris <147389885+PlusIce-Iris@users.noreply.github.com>
2023-11-26 13:41:40 +0100

    kayak

--- Assets/WaterWorks/Scripts/ControllerForce.cs
+++ Assets/WaterWorks/Scripts/ControllerForce.cs
@@ -18,20 +18,28 @@ public class ControllerForce : MonoBehaviour
     public SteamVR_Input_Sources rightSource;
     public SteamVR_Action_Pose rightAction = SteamVR_Input.GetAction<SteamVR_Action_Pose>("Pose");
     
-    public SteamVR_Input_Sources grabSource;
-    public SteamVR_Action_Boolean grabAction;
+    public SteamVR_Input_Sources clickSource;
+    public SteamVR_Action_Boolean clickAction;
+    //public SteamVR_Input_Sources grableftSource;
+   // public SteamVR_Action_Boolean grableftAction;
+    //public SteamVR_Input_Sources grabrightSource;
+    //public SteamVR_Action_Boolean grabrightAction;
     private float rotationSpeed = 2f;
     private bool isGrab;
+    //private GameObject paddleright;
     private void Start()
     {
         //rightMovement=GetComponent<ControllerRightMovement>();
+        //paddleright=GameObject.Find("Paddle");
+
         //leftMovement=GetComponent<ControllerLeftMovement>();
         
     }
     private void Update()
     {
-        Debug.Log(grabAction.GetStateUp(grabSource));
+        //Debug.Log(grabAction.GetStateUp(grabSource));
         // 获取手柄速度
+        
         rightVelocity=rightAction[rightSource].velocity;
         leftVelocity=leftAction[leftSource].velocity;
         rightForce = rightVelocity.magnitude;
@@ -43,23 +51,10 @@ public class ControllerForce : MonoBehaviour
     }
     void MoveObject()
     {
-        if (grabAction[grabSource].lastStateUp)
-        {
-            horizontalX=rightForce-leftForce;
-            forceZ=(rightForce+leftForce)/2;
+        
 
 
-            Vector3 moveDirection = new Vector3(0.2f*horizontalX, 0f, forceZ*0.2f);
-
-            // 使用手柄输入作为移动距离的比例因子，并乘以移动速度
-            Vector3 moveDistance = moveDirection * moveSpeed * Time.deltaTime;
-            transform.Rotate(Vector3.up, -horizontalX * rotationSpeed * Time.deltaTime);
-            // 应用移动
-            transform.Translate(moveDistance);
-            // 在这里执行抓取成功时的操作
-            Debug.Log("Grab action pressed down.");
-        }
-
+        /*
         if (grabAction.GetStateUp(grabSource))
         {
             // 在这里执行释放抓取时的操作
@@ -76,6 +71,38 @@ public class ControllerForce : MonoBehaviour
             transform.Translate(moveDistance);
             Debug.Log("Grab action released.");
         }
+        */
+        if (clickAction.GetState(clickSource))
+        {
+            // 在这里执行释放抓取时的操作
+            horizontalX=rightForce-leftForce;
+            forceZ=(rightForce+leftForce)/2;
+
+
+            Vector3 moveDirection = new Vector3(0.1f*horizontalX, 0f, forceZ*0.3f);
+
+            // 使用手柄输入作为移动距离的比例因子，并乘以移动速度
+            Vector3 moveDistance = moveDirection * moveSpeed * Time.deltaTime;
+            transform.Rotate(Vector3.up, -horizontalX * rotationSpeed * Time.deltaTime);
+            // 应用移动
+            transform.Translate(moveDistance);
+            Debug.Log("pas avance");
+        }
+        else { 
+            horizontalX=rightForce-leftForce;
+            forceZ=(rightForce+leftForce)/2;
+
+
+            Vector3 moveDirection = new Vector3(0.2f*horizontalX, 0f, -forceZ*0.5f);
+
+            // 使用手柄输入作为移动距离的比例因子，并乘以移动速度
+            Vector3 moveDistance = moveDirection * moveSpeed * Time.deltaTime;
+            transform.Rotate(Vector3.up, -horizontalX * rotationSpeed * Time.deltaTime);
+            // 应用移动
+            transform.Translate(moveDistance);
+            // 在这里执行抓取成功时的操作
+            Debug.Log("avance");
+        }
         // 根据手柄输入来计算移动向量
         /*
         horizontalX=rightForce-leftForce;
@@ -90,6 +117,6 @@ public class ControllerForce : MonoBehaviour
         // 应用移动
         transform.Translate(moveDistance);
         */
-       
+
     }
 }
